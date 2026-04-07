03d3dfb9c339a51fecec0e949861e31bb7815f47
Srijan Bajpai <srijan.bajpai29@gmail.com>
2026-04-07 07:13:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ export { default as RatingSite } from "./ratingsite.js";
 export { default as StudentManagement } from "./student-management.js";
 export { default as TaskManager } from "./taskmanager.js";
 export { default as RoutingProps } from "./routing-props.js";
-export { default as MenuProps } from "./restro menu props.js";
+export { default as MenuProps } from "./restro-menu-props.js";
